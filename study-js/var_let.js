85da40f0096b68ffcbebbb88dd7abc7ea88a4a5e
nayoon030303 <nayoon030303@gmail.com>
2021-07-19 11:03:58 +0900

    js, install react

---
+++ study-js/var_let.js
@@ -0,0 +1,9 @@
+//1et
+//let,const->블록 레벨 스코프
+//var->함수 레벨 스코프 
+
+let name = "을지문덕",age=33,email="test@test.com";
+
+//alert(name age email) //error
+alert(name+age+email);
+alert(name+" "+age+" "+email);
---
+++ study-js/var_variable.js
@@ -0,0 +1,14 @@
+//var,let,const 자바스크립트 변수 삼총사 
+
+//1. 변수란 무엇인가?
+//변수란 "값을 저장하는 저장공간" ==> 데이터를 저장하는 컴퓨터상의 메모리 공간
+bucket1 = 3;
+bucket2 = 6;
+
+result = bucket1+bucket2;
+
+window.alert(result);
+alert(result*3);
+
+
+//2. 변수 선언이란 무엇인가?
